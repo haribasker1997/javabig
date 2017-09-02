d28348825a5375ca385c8815e1b058e0ed25901e
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2017-09-02 20:42:40 +0530

    Add files via upload

---
+++ big.java
@@ -0,0 +1,21 @@
+#include java.io.*;
+#include java.util.*;
+class big
+{
+public static void main(String args[])
+{
+int a=10,b=15,c=18;
+if(a>b&&a>c)
+{
+System.out.println("a",+a);
+}
+elseif(b>c)
+{
+System.out.println("b",+b);
+}
+else
+{
+System.out.println("c",+c);
+}
+}
+}
